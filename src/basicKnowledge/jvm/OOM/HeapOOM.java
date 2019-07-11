df0c634ef4fbf4edbb39e3facec6bec1512d91f5
peicc1617 <peicc1413@outlook.com>
2019-07-11 19:22:17 +0800

    "第一次提交"

--- src/basicKnowledge/jvm/OOM/HeapOOM.java
+++ src/basicKnowledge/jvm/OOM/HeapOOM.java
@@ -4,11 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * VM Args:-Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
  * @program:summary
  * @author:peicc
  * @create:2019-07-11 17:21:27
  **/
-//Java堆OOM异常，主要不断创建对象，并且保证GC Roots到对象之间有可达路径来避免垃圾回收，在对象数量达到最大堆的容量时就会产生内存溢出异常
+//1、Java堆OOM异常，主要不断创建对象，并且保证GC Roots到对象之间有可达路径来避免垃圾回收，
+// 在对象数量达到最大堆的容量时就会产生内存溢出异常
+//2、设置Java堆的大小：-Xmx:堆的最小值 -Xmx:堆的最大值 -XX:+HeapDumpOnOutOfMemoryError
+// 在出现OOM异常时dump出当前内存转储快照
 public class HeapOOM {
     public static void main(String[] args) {
         System.out.println("测试开始");
