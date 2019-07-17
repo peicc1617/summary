ff8baed4c56af27e31d5243e918701e62075135a
peicc1617 <peicc1413@outlook.com>
2019-07-17 22:09:27 +0800

    "Collection"

---
+++ src/main/java/basicKnowledge/集合框架/CollectionTest.java
@@ -0,0 +1,12 @@
+package basicKnowledge.集合框架;
+
+import java.util.Collection;
+
+/**
+ * @program:summary
+ * @author:peicc
+ * @create:2019-07-17 21:52:29
+ **/
+public class CollectionTest {
+    Collection
+}
