0ecb1332217a84e39a7b3bab82b433f011b92717
peicc1617 <peicc1413@outlook.com>
2019-07-23 21:54:14 +0800

    "队列"

---
+++ src/main/java/basicKnowledge/集合框架/queue/TestQueue.java
@@ -0,0 +1,13 @@
+package basicKnowledge.集合框架.queue;
+
+import java.util.Queue;
+
+/**
+ * @基本功能:
+ * @program:summary
+ * @author:peicc
+ * @create:2019-07-23 21:48:43
+ **/
+public class TestQueue {
+
+}
