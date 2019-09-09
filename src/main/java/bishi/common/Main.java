bbece0d2bc02de6d381c4340547e22fa34473761
peicc1617 <peicc1413@outlook.com>
2019-09-09 09:38:53 +0800

    "就是git"

--- src/main/java/bishi/common/Main.java
+++ src/main/java/bishi/common/Main.java
@@ -1,5 +1,6 @@
 package bishi.common;
 
+import java.util.HashMap;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
@@ -7,8 +8,8 @@ import java.util.concurrent.locks.Lock;
 public class Main {
     public final static ThreadLocal<String> s = new ThreadLocal<String>();
     public static void main(String[] args) {
-       int n=Runtime.getRuntime().availableProcessors();
-        System.out.println(n);
+        int[] arr=new int[5];
+        System.out.println(arr[0]);
     }
 
 }
