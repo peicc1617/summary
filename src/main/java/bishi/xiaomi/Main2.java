97da3010701d4803da151fbd556ac7193f0004cd
peicc <929594275@qq.com>
2019-09-11 18:45:49 +0800

    xiaomi

---
+++ src/main/java/bishi/xiaomi/Main2.java
@@ -0,0 +1,10 @@
+package bishi.xiaomi;
+
+/**
+ * @基本功能:
+ * @program:summary
+ * @author:peicc
+ * @create:2019-09-11 18:45:25
+ **/
+public class Main2 {
+}
---
+++ src/main/java/bishi/xiaomi/Main2_2.java
@@ -0,0 +1,10 @@
+package bishi.xiaomi;
+
+/**
+ * @基本功能:
+ * @program:summary
+ * @author:peicc
+ * @create:2019-09-11 18:45:34
+ **/
+public class Main2_2 {
+}
