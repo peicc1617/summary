4e7111b141c32992f94d6f3d5b2a5e4397e673d4
peicc1617 <peicc1413@outlook.com>
2019-09-11 20:07:53 +0800

    "xiaomi2"

--- src/main/java/bishi/xiaomi/Main1_1.java
+++ src/main/java/bishi/xiaomi/Main1_1.java
@@ -1,5 +1,8 @@
 package bishi.xiaomi;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * @基本功能:
  * @program:summary
@@ -7,4 +10,27 @@ package bishi.xiaomi;
  * @create:2019-09-11 18:45:17
  **/
 public class Main1_1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        int[] sortArr = new int[n];
+        for (int i = 0; i < n; i++) {
+
+            arr[i] = scanner.nextInt();
+            sortArr[i] = arr[i];
+        }
+        Arrays.sort(sortArr);
+        int count = 0;
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+
+            if(arr[i] == sortArr[j]){
+                ++count;
+                ++j;
+            }
+        }
+        System.out.println(n-count);
+    }
 }
---
+++ src/main/java/bishi/xiaomi/Test.java
@@ -0,0 +1,9 @@
+package bishi.xiaomi;
+
+/**
+ * @基本功能:
+ * @program:summary
+ * @author:peicc
+ * @create:2019-09-11 19:28:18
+ **/
+
