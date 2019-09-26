5199d32d848e5f469880b1545ad06ebd73a03ea5
peicc1617 <peicc1413@outlook.com>
2019-09-26 20:57:17 +0800

    360-2

--- src/main/java/bishi/_360/Main1.java
+++ src/main/java/bishi/_360/Main1.java
@@ -1,5 +1,8 @@
 package bishi._360;
 
+import java.util.LinkedList;
+import java.util.Scanner;
+
 /**
  * @基本功能:
  * @program:summary
@@ -7,4 +10,33 @@ package bishi._360;
  * @create:2019-09-26 18:51:03
  **/
 public class Main1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for (int i = 0; i <n ; i++) {
+            arr[i]=sc.nextInt();
+        }
+        int[][] res=new int[n][2];
+        LinkedList<Integer> stack=new LinkedList<>();//构造一个单调递减栈
+        for (int i = 0; i <arr.length ; i++) {
+            while(!stack.isEmpty()&&arr[stack.peek()]>=arr[i]){//待插入元素小于等于栈顶元素
+                res[stack.peek()][1]=i;
+                stack.pop();
+            }
+            if(!stack.isEmpty()){
+                res[i][0]=stack.peek();
+            }else{
+                res[i][0]=-1;
+            }
+            stack.push(i);
+        }
+        while(!stack.isEmpty()){
+            res[stack.pop()][1]=-1;
+        }
+        for (int i = 0; i <res.length ; i++) {
+            System.out.println(res[i][0]+" "+res[i][1]);
+        }
+    }
+
 }
