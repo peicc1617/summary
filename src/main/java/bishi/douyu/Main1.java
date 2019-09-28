319716678fe06efe63520d83fab3259db39d27a7
peicc1617 <peicc1413@outlook.com>
2019-09-28 14:48:13 +0800

    斗鱼

---
+++ src/main/java/bishi/douyu/Main1.java
@@ -0,0 +1,15 @@
+package bishi.douyu;
+
+import java.util.Scanner;
+
+/**
+ * @基本功能:
+ * @program:summary
+ * @author:peicc
+ * @create:2019-09-28 14:35:35
+ **/
+public class Main1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+    }
+}
---
+++ src/main/java/bishi/douyu/Main2.java
@@ -0,0 +1,10 @@
+package bishi.douyu;
+
+/**
+ * @基本功能:
+ * @program:summary
+ * @author:peicc
+ * @create:2019-09-28 14:35:55
+ **/
+public class Main2 {
+}
