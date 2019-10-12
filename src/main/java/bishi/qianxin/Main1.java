2387cc46bf93b4616523739677dfde5b4a2d55e9
peicc <929594275@qq.com>
2019-10-12 20:22:28 +0800

    qianxin_finish

--- src/main/java/bishi/qianxin/Main1.java
+++ src/main/java/bishi/qianxin/Main1.java
@@ -1,5 +1,7 @@
 package bishi.qianxin;
 
+import java.util.Scanner;
+
 /**
  * @基本功能:
  * @program:summary
@@ -7,4 +9,48 @@ package bishi.qianxin;
  * @create:2019-10-12 18:51:02
  **/
 public class Main1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=0;
+        try{
+            n=sc.nextInt();
+
+        }catch (Exception e){
+            System.out.println("错误输入");
+            return;
+        }
+        getResult(n);
+
+
+    }
+    public static void getResult(int n){
+        String[] arr={"零","一","二","三","四","五","六","七","八","九"};
+        String[] arr1={"","十","百","千","万"};
+        String str=String.valueOf(n);
+        char[] ch=str.toCharArray();
+        StringBuilder sb=new StringBuilder();
+        for (int i = 0; i <ch.length ; i++) {
+            int index=ch[i]-48;
+            if(index!=0){
+                sb.append(arr[index]).append(arr1[ch.length-i-1]);
+            }else{
+                if(i==ch.length-1){
+
+                }else{
+                    if (ch[i+1]!='0'){
+                        sb.append(arr[index]);
+                    }
+                }
+
+            }
+        }
+        /*String temp=new String(sb);
+        char[] tempArr=temp.toCharArray();
+        for (int i = 0; i <tempArr.length ; i++) {
+            if(){
+
+            }
+        }*/
+        System.out.println(sb.toString());
+    }
 }
--- src/main/java/bishi/qianxin/Main2.java
+++ src/main/java/bishi/qianxin/Main2.java
@@ -1,5 +1,7 @@
 package bishi.qianxin;
 
+import java.util.Scanner;
+
 /**
  * @基本功能:
  * @program:summary
@@ -7,4 +9,36 @@ package bishi.qianxin;
  * @create:2019-10-12 18:51:41
  **/
 public class Main2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i]=sc.nextInt();
+        }
+        int q=sc.nextInt();
+        for (int i = 0; i <q ; i++) {
+            int L=sc.nextInt();
+            int R=sc.nextInt();
+            int K=sc.nextInt();
+            hit(arr,L,R,K);
+        }
+    }
+    public static void hit(int[] arr,int L,int R,int K){
+        int left=L-1;
+        int right=R-1;
+        if(left<0){
+            left=0;
+        }
+        if(right>=arr.length){
+            right=arr.length-1;
+        }
+        int res=0;
+        for (int i = left; i <=right ; i++) {
+            if(arr[i]==K){
+                res++;
+            }
+        }
+        System.out.println(res);
+    }
 }
