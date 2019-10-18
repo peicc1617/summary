a626f9595f501eea69405eca98f98ba76c02287d
peicc <929594275@qq.com>
2019-10-18 13:56:29 +0800

    ebay-finish

--- src/main/java/bishi/ebay/Main1.java
+++ src/main/java/bishi/ebay/Main1.java
@@ -1,5 +1,8 @@
 package bishi.ebay;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * @基本功能:
  * @program:summary
@@ -7,4 +10,27 @@ package bishi.ebay;
  * @create:2019-10-17 18:33:43
  **/
 public class Main1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int W=sc.nextInt();
+        int[] weight=new int[n];
+        for (int i = 0; i <n ; i++) {
+            weight[i]=sc.nextInt();
+        }
+        Arrays.sort(weight);
+        int i=0;
+        int j=n-1;
+        int res=0;
+        while (i<=j){
+            if (weight[i]+weight[j]<=W){
+                i++;
+                j--;
+            }else{
+                j--;
+            }
+            res++;
+        }
+        System.out.println(res);
+    }
 }
--- src/main/java/bishi/ebay/Main2.java
+++ src/main/java/bishi/ebay/Main2.java
@@ -1,5 +1,9 @@
 package bishi.ebay;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
 /**
  * @基本功能:
  * @program:summary
@@ -7,4 +11,58 @@ package bishi.ebay;
  * @create:2019-10-17 18:34:16
  **/
 public class Main2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int M=sc.nextInt();
+        int K=sc.nextInt();
+        int[] arr=new int[K];
+        for (int i = 0; i <K ; i++) {
+            arr[i]=sc.nextInt();
+        }
+        ArrayList list=new ArrayList();
+        int count=0;
+        boolean flag=true;
+        for (int i = 0; i <K ; i++) {
+            if (!list.contains(arr[i])){
+                if (list.size()<M){
+                    list.add(arr[i]);
+                    count++;
+                }else{
+                    flag=true;
+                    int postion=i+1;
+                    for (int j = 0; j <list.size() ; j++) {
+                        int temp=(Integer) list.get(j);
+                        int index=getIndex(arr,i+1,temp);
+                        if (index==-1){
+                            list.remove((Integer)j);
+                            list.add(arr[i]);
+                            count++;
+                            flag=false;
+                            break;
+                        }else{
+                            if (index>postion){
+                                postion=index;
+                            }
+                        }
+                    }
+                    if(flag){
+                        list.remove((Integer)arr[postion]);
+                        list.add(arr[i]);
+                        count++;
+                    }
+
+                }
+            }
+        }
+        System.out.println(count);
+    }
+    public static int getIndex(int[] arr,int start,int target){
+        for (int i = start; i <arr.length ; i++) {
+            if (arr[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
